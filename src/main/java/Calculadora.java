01b15b4b08d168e9a92887afe99acb7b8f2a2296
fernandorff <fernandorff92@gmail.com>
2022-08-13 14:23:53 -0300

    a

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,61 +1,70 @@
 public class Calculadora {
 
+    double A = 0;
+    double B = 0;
+    double C = 0;
 
-    public void soma (double A, double B){
+
+    public double soma (){
         double resultado = A + B;
         System.out.println(A + " + " + B + " = " + resultado);
+        return resultado;
 
 
 
     }
 
-    public void subtracao (double A, double B){
+    public double subtracao (){
         double resultado = A - B;
         System.out.println(A + " - " + B + " = " + resultado);
+        return resultado;
 
     }
 
-    public void multiplicacao (double A, double B){
+    public double multiplicacao (){
         double resultado = A * B;
         System.out.println(A + " * " + B + " = " + resultado);
+        return resultado;
 
     }
 
-    public void divisao (double A, double B){
+    public double divisao (){
         double resultado = A / B;
         System.out.println(A + " / " + B + " = " + resultado);
+        return resultado;
 
     }
 
-    public void potenciacao (double A, double B){
-        double resultado = Math.pow(A,B);
-        System.out.println(A + " ** " + B + " = " + resultado);
+//    public double potenciacao (){
+//        double resultado = Math.pow(A,B);
+//        System.out.println(A + " ** " + B + " = " + resultado);
+//        return resultado;
+//
+//    }
 
-    }
-
-    public double bhaskara (double A, double B, double C){
-        double delta = Math.pow(B,2) - (4 * A * C);
-        double x1; double x2 ;
-        double[] resultado = new double[2];
-
-        // Delta negativo nao apresenta raizes reais.
-        if (delta < 0){
-            System.out.println(A + "x² + " + B + "x + " + C);
-            System.out.println("Delta = " + Math.pow(delta, 1));
-            System.out.println("X não tem valor real.");
-            return x1;
-        }
-        else {
-            x1 = ((B * -1) + Math.sqrt(delta))/(2*A);
-            x2 = ((B * -1) - Math.sqrt(delta))/(2*A);
-            System.out.println(A + "x² + " + B + "x + " + C);
-            System.out.println("Delta = " + Math.pow(delta, 1));
-            System.out.println("x' = " + x1 + "   x'' = " + x2);
-        }
-
-
-        return x1;
-    }
+//    public double bhaskara (){
+//        double delta = Math.pow(B,2) - (4 * A * C);
+//        double x1; double x2;
+//            int[] resultado = new int[2];
+//
+//        // Delta negativo nao apresenta raizes reais.
+//        if (delta < 0){
+//            System.out.println(A + "x² + " + B + "x + " + C);
+//            System.out.println("Delta = " + Math.pow(delta, 1));
+//            System.out.println("X não tem valor real.");
+//            return x1;
+//        }
+//        else {
+//            x1 = ((B * -1) + Math.sqrt(delta))/(2*A);
+//            x2 = ((B * -1) - Math.sqrt(delta))/(2*A);
+//            System.out.println(A + "x² + " + B + "x + " + C);
+//            System.out.println("Delta = " + Math.pow(delta, 1));
+//            System.out.println("x' = " + x1 + "   x'' = " + x2);
+//        }
+//
+//
+//        return x1;
+//    }
 
 
 
--- src/test/java/TestCode.java
+++ src/test/java/TestCode.java
@@ -5,58 +5,78 @@ public class TestCode {
 
 
     @Test
-    public void testarSoma(){
+    public void deveSomarCorretamenteQuandoOsValoresForemInteiros(){
         Calculadora calculadora1 = new Calculadora();
-        calculadora1.soma(6,3);
+        calculadora1.A = 6;
+        calculadora1.B = 3;
 
+        double valorEsperado = calculadora1.A + calculadora1.B;
+        double valorCalculado = calculadora1.soma();
+
+        Assert.assertEquals(valorEsperado, valorCalculado, 0.01);
     }
 
     @Test
     public void testarSubtracao(){
         Calculadora calculadora1 = new Calculadora();
-        calculadora1.subtracao(6,3);
+        calculadora1.A = 6;
+        calculadora1.B = 3;
 
-    }
+        double valorEsperado = calculadora1.A - calculadora1.B;
+        double valorCalculado = calculadora1.subtracao();
 
-    @Test
-    public void testarMultiplicacao(){
-        Calculadora calculadora1 = new Calculadora();
-        calculadora1.multiplicacao(6,3);
-
-    }
-
-    @Test
-    public void testarDivisao(){
-        Calculadora calculadora1 = new Calculadora();
-        calculadora1.divisao(6,3);
-
-    }
-
-    @Test
-    public void testarPotenciacao(){
-        // Arrange
-        Calculadora calculadora1 = new Calculadora();
-        calculadora1.potenciacao(6,3);
-        double valorEsperado = Math.pow(6,3);
-
-        // Act
-        double valorCalculado = calculadorComissao.calcularValorComissao();
-
-
-
-        // Assert
         Assert.assertEquals(valorEsperado, valorCalculado, 0.01);
 
+    }
+
+    @Test
+    public void deveMultiplicarCorretamenteQuandoNumerosForemInteiros(){
+        Calculadora calculadora1 = new Calculadora();
+        calculadora1.A = 6;
+        calculadora1.B = 3;
+
+        double valorEsperado = calculadora1.A * calculadora1.B;
+        double valorCalculado = calculadora1.multiplicacao();
+
+        Assert.assertEquals(valorEsperado, valorCalculado, 0.01);
 
     }
 
     @Test
-    public void testarBhaskara(){
+    public void deveDividirCorretamenteQuandoNumerosForemInteiros(){
         Calculadora calculadora1 = new Calculadora();
-        calculadora1.bhaskara(4,3, 2);
+        calculadora1.A = 6;
+        calculadora1.B = 3;
 
+        double valorEsperado = calculadora1.A / calculadora1.B;
+        double valorCalculado = calculadora1.divisao();
+
+        Assert.assertEquals(valorEsperado, valorCalculado, 0.01);
 
     }
+
+//    @Test
+//    public void testarPotenciacao(){
+//        // Arrange
+//        Calculadora calculadora1 = new Calculadora();
+//        calculadora1.A = 6;
+//        calculadora1.B = 3;
+//
+//        double valorEsperado = Math.pow(calculadora1.A, calculadora1.B);
+//        double valorCalculado = calculadora1.potenciacao();
+//
+//        Assert.assertEquals(valorEsperado, valorCalculado, 0.01);
+//
+//
+//    }
+
+//    @Test
+//    public void testarBhaskara(){
+//        Calculadora calculadora1 = new Calculadora();
+//        calculadora1.bhaskara(4,3, 2);
+//
+//
+//    }
 
 
 }
