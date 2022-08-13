98ea67540cb7dae64e900dc1b627aa053f848211
fernandorff <fernandorff92@gmail.com>
2022-08-13 15:28:06 -0300

    limpando code smells

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,40 +1,42 @@
 public class Calculadora {
 
-    double A = 0;
-    double B = 0;
-    double C = 0;
+    double valorA = 0;
+    double valorB = 0;
+    double valorC = 0;
 
 
     public double soma() {
-        return A + B;
+        return valorA + valorB;
 
     }
 
     public double subtracao() {
-        return A - B;
+        return valorA - valorB;
 
     }
 
     public double multiplicacao() {
-        return A * B;
+        return valorA * valorB;
 
     }
 
     public double divisao() {
-        return A / B;
+        return valorA / valorB;
 
     }
 
     public double potenciacao() {
-        return Math.pow(A, B);
+        return Math.pow(valorA, valorB);
 
     }
 
     public double bhaskara (){
-        double delta = Math.pow(B,2) - (4 * A * C);
-        double x1; double x2;
-        x1 = ((B * -1) + Math.sqrt(delta))/(2*A);
-        x2 = ((B * -1) - Math.sqrt(delta))/(2*A);
+        double delta = Math.pow(valorB,2) - (4 * valorA * valorC);
+        double x1;
+        double x2;
+        x1 = ((valorB * -1) + Math.sqrt(delta))/(2* valorA);
+        x2 = ((valorB * -1) - Math.sqrt(delta))/(2* valorA);
         return x1 + x2;
+
     }
 }
--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -3,14 +3,13 @@ import org.junit.Test;
 
 public class CalculadoraTest {
 
-
     @Test
     public void deveSomarCorretamenteQuandoOsValoresForemInteiros(){
         Calculadora calculadora1 = new Calculadora();
-        calculadora1.A = 6;
-        calculadora1.B = 3;
+        calculadora1.valorA = 6;
+        calculadora1.valorB = 3;
 
-        double valorEsperado = calculadora1.A + calculadora1.B;
+        double valorEsperado = calculadora1.valorA + calculadora1.valorB;
         double valorCalculado = calculadora1.soma();
 
         Assert.assertEquals(valorEsperado, valorCalculado, 0.01);
@@ -19,83 +18,79 @@ public class CalculadoraTest {
     @Test
     public void deveSubtrairCorretamenteQuandoNumerosPossuemPontosFlutuantes(){
         Calculadora calculadora1 = new Calculadora();
-        calculadora1.A = 6;
-        calculadora1.B = 3;
+        calculadora1.valorA = 6;
+        calculadora1.valorB = 3;
 
-        double valorEsperado = calculadora1.A - calculadora1.B;
+        double valorEsperado = calculadora1.valorA - calculadora1.valorB;
         double valorCalculado = calculadora1.subtracao();
 
         Assert.assertEquals(valorEsperado, valorCalculado, 0.01);
-
     }
 
     @Test
     public void deveMultiplicarCorretamenteQuandoNumerosForemInteiros(){
         Calculadora calculadora1 = new Calculadora();
-        calculadora1.A = 6;
-        calculadora1.B = 3;
+        calculadora1.valorA = 6;
+        calculadora1.valorB = 3;
 
-        double valorEsperado = calculadora1.A * calculadora1.B;
+        double valorEsperado = calculadora1.valorA * calculadora1.valorB;
         double valorCalculado = calculadora1.multiplicacao();
 
         Assert.assertEquals(valorEsperado, valorCalculado, 0.01);
-
     }
 
     @Test
     public void deveDividirCorretamenteQuandoNumerosForemInteiros(){
         Calculadora calculadora1 = new Calculadora();
-        calculadora1.A = 6;
-        calculadora1.B = 3;
+        calculadora1.valorA = 6;
+        calculadora1.valorB = 3;
 
-        double valorEsperado = calculadora1.A / calculadora1.B;
+        double valorEsperado = calculadora1.valorA / calculadora1.valorB;
         double valorCalculado = calculadora1.divisao();
 
         Assert.assertEquals(valorEsperado, valorCalculado, 0.01);
-
     }
 
     @Test
     public void deveDividirCorretamenteQuandoNumerosPossuemPontosFlutuantes(){
         Calculadora calculadora1 = new Calculadora();
-        calculadora1.A = 6;
-        calculadora1.B = 3;
+        calculadora1.valorA = 6;
+        calculadora1.valorB = 3;
 
-        double valorEsperado = calculadora1.A / calculadora1.B;
+        double valorEsperado = calculadora1.valorA / calculadora1.valorB;
         double valorCalculado = calculadora1.divisao();
 
         Assert.assertEquals(valorEsperado, valorCalculado, 0.01);
-
     }
 
     @Test
     public void devePotencializarCorretamenteQuandoNumerosForemInteiros(){
         // Arrange
         Calculadora calculadora1 = new Calculadora();
-        calculadora1.A = 6;
-        calculadora1.B = 3;
+        calculadora1.valorA = 6;
+        calculadora1.valorB = 3;
 
-        double valorEsperado = Math.pow(calculadora1.A, calculadora1.B);
+        double valorEsperado = Math.pow(calculadora1.valorA, calculadora1.valorB);
         double valorCalculado = calculadora1.potenciacao();
 
         Assert.assertEquals(valorEsperado, valorCalculado, 0.01);
-
-
     }
 
     @Test
-    public void deveCalcularSomaDeX1EX2UsandoBhaskaraCorretamenteQuandoNumerosForemInteiros(){
+    public void deveCalcularSomaDeX1eX2UsandoBhaskaraCorretamenteQuandoNumerosForemInteiros(){
         Calculadora calculadora1 = new Calculadora();
-        calculadora1.A = 4;
-        calculadora1.B = 3;
-        calculadora1.C = 1;
+        calculadora1.valorA = 3;
+        calculadora1.valorB = 6;
+        calculadora1.valorC = 1;
 
-        double valorEsperado = (((calculadora1.B * -1) + Math.sqrt(Math.pow(calculadora1.B,2) - (4 * calculadora1.A * calculadora1.C)))/(2* calculadora1.A)) +
-                (((calculadora1.B * -1) - Math.sqrt(Math.pow(calculadora1.B,2) - (4 * calculadora1.A * calculadora1.C)))/(2* calculadora1.A));
+        double valorEsperado =
+                (((calculadora1.valorB * -1) + Math.sqrt(Math.pow(calculadora1.valorB,2) - (4 * calculadora1.valorA * calculadora1.valorC)))/(2* calculadora1.valorA))
+                +
+                (((calculadora1.valorB * -1) - Math.sqrt(Math.pow(calculadora1.valorB,2) - (4 * calculadora1.valorA * calculadora1.valorC)))/(2* calculadora1.valorA));
         double valorCalculado = calculadora1.bhaskara();
+        System.out.println(valorCalculado);
 
         Assert.assertEquals(valorEsperado, valorCalculado, 0.01);
-
     }
 
 
